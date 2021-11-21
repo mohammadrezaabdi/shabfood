c15ba1c180ba99e08aac5b87b7fab30f71520ed3
mohammadrezaabdi <wnick2085@gmail.com>
2021-11-22 03:14:19 +0330

    added nginx/html directory

---
+++ nginx/html/precache-manifest.455b4cf2add2600f0c6c2d952ff86fff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "189c7b8ac39bdd63f30188ba189d6f37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddb3b3d1414717b24ffc",
+    "url": "/static/css/2.534f228d.chunk.css"
+  },
+  {
+    "revision": "ddb3b3d1414717b24ffc",
+    "url": "/static/js/2.3efebfc1.chunk.js"
+  },
+  {
+    "revision": "54f9f0e872e920f182f7f5c792c85756",
+    "url": "/static/js/2.3efebfc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c846df339239a6afebc9",
+    "url": "/static/js/3.23cad79a.chunk.js"
+  },
+  {
+    "revision": "45e11ca0eeae411d8be6",
+    "url": "/static/js/main.091520b7.chunk.js"
+  },
+  {
+    "revision": "1a3834c950bac323ea6b",
+    "url": "/static/js/runtime-main.ab12ed95.js"
+  }
+]);
